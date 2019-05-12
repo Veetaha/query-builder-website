937c61a89de28d42d7436dd2d415d925aa345f90
Veetaha <gerzoh1@gmail.com>
2019-05-12 20:12:28 +0300

    added isOpenned flag to createProposal() api input type

--- backend/src/modules/proposal/gql/proposal-create.input.ts
+++ backend/src/modules/proposal/gql/proposal-create.input.ts
@@ -1,9 +1,10 @@
+import { Nullable  } from 'ts-typedefs';
 import { InputType } from 'type-graphql';
 
-import * as I from '@app/interfaces';
-import { StringField } from '@utils/gql/decorators/explicit-type-field.decorator';
+import { StringField, BooleanField } from '@utils/gql/decorators/explicit-type-field.decorator';
 import { ValidateAs  } from '@utils/validation/validations.decorator';
 import { NullableOpt } from '@utils/gql/opts';
+
 import { Proposal    } from '../proposal.entity';
 
 @InputType()
@@ -13,8 +14,10 @@ export class ProposalCreateInput implements Partial<Proposal> {
     @ValidateAs(Proposal, 'introText') @StringField() introText!: string;
     @ValidateAs(Proposal, 'bodyText')  @StringField() bodyText!:  string;
 
-    @ValidateAs(Proposal, 'mainPictureUrl')
+    @BooleanField() isOpenned!: boolean;
+
+    @ValidateAs(Proposal, 'mainPictureUrl') 
     @StringField(NullableOpt)
-    mainPictureUrl?: I.Nullable<string>;
+    mainPictureUrl?: Nullable<string>;
 
 }
